74a4f11da0e1f521a3d6b02f77211143992b237d
Alessandro Agnelli <agnelli.jedi@gmail.com>
2020-10-15 22:52:31 +0200

    Add vscode keybindings

--- install.sh
+++ install.sh
@@ -6,5 +6,7 @@ for file in vimrc zshrc tmux.conf; do
 	ln -s "${BASEDIR}/$file" ~/.$file
 done
 
+ln -s "${BASEDIR}/vscode/keybindings.json" ~/.config/Code/User/keybindings.json
+
 git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
