c1d78a4b803b68fc149e785d9b10f2a17c694000
Alessandro Agnelli <agnelli.jedi@gmail.com>
2020-10-15 12:54:31 +0200

    Add installation script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+for file in vimrc zshrc tmux.conf; do
+	ln -s "${BASEDIR}/$file" ~/.$file
+done
+
+git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
